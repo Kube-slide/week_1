3c219603ce3b819ad5cee95823422f960d258341
KubeSlide <110945154+Kube-slide@users.noreply.github.com>
2025-08-27 11:20:01 -0400

    Implemented spikes
    - Added spike object to game
    - Added ability to reset level
    - Added more control towards level properties

---
+++ Scripts/Rotate_Level.gd
@@ -0,0 +1,17 @@
+extends Node2D
+@export var rotateStrength : float = 1
+var playerInputs : float = 0
+@export var rotateSmoothing : float = 2
+var rotationEnabled : bool = true
+
+func _physics_process(delta):
+	if rotationEnabled:
+		if Input.get_accelerometer() == Vector3.ZERO:
+			playerInputs = lerpf(playerInputs, Input.get_action_strength("Rotate_right") - Input.get_action_strength("Rotate_left"), delta * rotateSmoothing)
+		else:
+			playerInputs = lerpf(playerInputs, clamp(round(Input.get_accelerometer().x / 2), -1, 1), rotateSmoothing * delta)
+			print("Rotation speed: " + str(playerInputs) + " | Device sensor: " + str(Input.get_accelerometer().length()))
+	rotate(deg_to_rad(playerInputs) * rotateStrength) 
+
+func ResetRotation() -> void:
+	rotation = 0
---
+++ Scripts/ball_grounded_check.gd
@@ -0,0 +1,27 @@
+extends RigidBody2D
+@export var spawnLoc : Node2D
+@onready var deathTimer : Timer = $DeathTimer
+@export var worldMap : Node2D
+var deathFlag : bool = false
+
+func _on_body_entered(body: Node) -> void:
+	if body.collision_layer == 2 && !deathFlag:
+		deathFlag = true
+		linear_velocity = Vector2.ZERO
+		angular_velocity = 0
+		visible = !visible
+		set_deferred("freeze", true)
+		deathTimer.start()
+		worldMap.rotationEnabled = false
+
+func ResetPos() -> void:
+	position = spawnLoc.global_position
+	visible = !visible
+	set_deferred("freeze", false)
+	deathFlag = false
+	worldMap.ResetRotation()
+	worldMap.rotationEnabled = true
+
+
+func _on_death_timer_timeout() -> void:
+	ResetPos()
