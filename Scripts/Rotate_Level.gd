fd0bea97aa96ae78f76ca139a0031c14fd6103d9
KubeSlide <rajaabdul006@gmail.com>
2025-08-28 21:46:16 -0400

    Added more platforms to test level
    - Added some extra platforms on the test level to test out multi-surface rotation and dummy level design

--- Scripts/Rotate_Level.gd
+++ Scripts/Rotate_Level.gd
@@ -12,7 +12,7 @@ func _physics_process(delta):
 		if Input.get_accelerometer() == Vector3.ZERO:
 			playerInputs = lerpf(playerInputs, Input.get_action_strength("Rotate_right") - Input.get_action_strength("Rotate_left"), delta * rotateSmoothing)
 		else:
-			playerInputs = lerpf(playerInputs, clamp(round(Input.get_accelerometer().x / 2), -1, 1), rotateSmoothing * delta)
+			playerInputs = lerpf(playerInputs, clamp(Input.get_accelerometer().x / 3, -1, 1), rotateSmoothing * delta)
 			print("Rotation speed: " + str(playerInputs) + " | Device sensor: " + str(Input.get_accelerometer().length()))
 	rotate(deg_to_rad(playerInputs) * rotateStrength * delta)
 	if resetRot:
